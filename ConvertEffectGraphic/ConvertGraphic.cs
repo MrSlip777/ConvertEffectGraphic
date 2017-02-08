d3e399fb6004e75af21de79f5a03115eada11b4f
MrSlip777 <mrslipgamecreate01@gmail.com>
2017-02-08 21:44:24 +0900

    begin

---
+++ ConvertEffectGraphic/ConvertGraphic.cs
@@ -0,0 +1,105 @@
+﻿/*
+*参考URL：http://qiita.com/Toshi332/items/2749690489730f32e63d
+*/
+
+using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ConvertEffectGraphic
+{
+    class ConvertGraphic
+    {
+        private readonly int const_original_size = 240;    //元のサイズ
+        private readonly int const_convert_size = 192;     //変換後のサイズ
+        private readonly string const_folder_name = "\\convert\\";
+        private readonly double const_CalcCorrection = 0.1;
+
+        //※画像に対して横幅のフレーム数
+        private const int const_Width_framenumber = 5;     //幅フレーム数 
+
+        public string ReadImage(string inputPath) {
+
+            try
+            {
+                int width, height;
+                //追記：元画像のフォーマット
+                ImageFormat format;
+
+                //元画像読み込み
+                Bitmap img = new Bitmap(Image.FromFile(inputPath));
+                //元画像のフォーマットを保持
+                format = img.RawFormat;
+
+                //画像サイズを取得
+                width = img.Width;
+                height = img.Height;
+
+                //サイズ変換
+                Bitmap resizeImg
+                    = new Bitmap(img,
+                    (int)((float)width * (float)const_convert_size / (float)const_original_size),
+                    (int)((float)height * (float)const_convert_size / (float)const_original_size));
+
+                //出力先画像を用意
+                Bitmap convertImg
+                    = new Bitmap(
+                    const_convert_size * const_Width_framenumber,
+                    (int)Math.Ceiling((double)resizeImg.Width
+                    / (double)const_convert_size / (double)const_Width_framenumber)
+                    * const_convert_size);
+
+                //リサイズ画像から写し取る
+                for (int x = 0; x < resizeImg.Width; x++)
+                {
+
+                    for (int y = 0; y < resizeImg.Height; y++)
+                    {
+                        int convert_x = x % (const_convert_size * const_Width_framenumber);
+                        int convert_y
+                            = y + ((int)Math.Ceiling((double)((double)x + const_CalcCorrection)
+                            / (double)const_convert_size / (double)const_Width_framenumber)
+                            - 1) * const_convert_size;
+
+                        convertImg.SetPixel(convert_x, convert_y, resizeImg.GetPixel(x, y));
+                    }
+                }
+
+                //出力先フォルダー生成
+                string folder_Path = null;
+
+                FileInfo fileInfo = new FileInfo(inputPath);
+                folder_Path = fileInfo.DirectoryName + const_folder_name;
+
+                if (!Directory.Exists(folder_Path))
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(folder_Path);
+                    directoryInfo.Create();
+                }
+
+                //ファイルパス
+                string outputPath = folder_Path + fileInfo.Name;
+
+                //同じファイルが出力先にない場合出力される
+                if (!File.Exists(outputPath)) {
+                    //元画像のフォーマットで保存する
+                    convertImg.Save(outputPath, format);
+                }
+
+                //画像メモリを開放する
+                img.Dispose();
+                resizeImg.Dispose();
+                convertImg.Dispose();
+
+                return "success!";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
+
+    }
+}
---
+++ ConvertEffectGraphic/Form1.cs
@@ -0,0 +1,89 @@
+﻿/*
+*参考URL：http://dobon.net/vb/dotnet/file/getfiles.html
+*/
+using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ConvertEffectGraphic
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 各種ボタン処理
+        /// </summary>
+        /// 
+
+        private readonly string sErrorFolderPath = "入力フォルダパスが正しくありません。";
+        private readonly string sSuccess = "success!";
+
+        //インスタンス定義
+        static private ConvertGraphic mConvertGraphic = new ConvertGraphic();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_open_input_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        //画像変換の実施
+        private void button_Implement_Click(object sender, EventArgs e)
+        {
+            string sOutputMessage = null;
+
+            if (textBox1.Text == "")
+            {
+                sOutputMessage = sErrorFolderPath;
+            }
+
+            if (sOutputMessage != sErrorFolderPath) {
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(textBox1.Text);
+                FileInfo[] files
+                    = directoryInfo.GetFiles("*", SearchOption.TopDirectoryOnly);
+
+                foreach (FileInfo file in files)
+                {
+                    sOutputMessage = mConvertGraphic.ReadImage(file.FullName);
+
+                    //エラーが発生したら中断する
+                    if (sOutputMessage != sSuccess)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            //出力結果をダイアログ表示
+            if (sOutputMessage == sSuccess) {
+
+                MessageBox.Show(
+                    "変換完了",
+                    "出力結果",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(
+                   sOutputMessage,
+                   "出力結果",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
